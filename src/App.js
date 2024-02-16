7e657872ad8405927eabfbf111048cde362b0eb9
keep <flymayday112@gmail.com>
2024-02-16 19:49:01 +0800

    add initial useState to input 2 elements for return within funciton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-     {/* 會在這裡添加待辦事項列表的代碼 */}
+     {/* 會在這裡添加待辦事項列表的UI界面代碼 */}
     </div>
   );
 }
