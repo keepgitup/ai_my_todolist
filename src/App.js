b42b84b104845f2cab12ceb9ecea8c5a6d80ebd1
keep <flymayday112@gmail.com>
2024-02-17 19:16:29 +0800

    add the map function to let each item involved in index then transfer into the number or coordinates value stored in the key

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  //...之前的狀態
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
-  /* 添加一個輸入字段以及一個按鈕來添加新的待辦事項。 */
-  return (
-    <div className="App">{/* 會在這裡添加待辦事項列表的UI界面代碼 */}
-    <input type="text"
-    value={todo}
-    onChange={(e)=> setTodo(e.target.value)}
-    />
-<button onClick={()=>{}}
->
 
-</button>
+  /* 添加一個輸入字段以及一個按鈕來添加新的待辦事項。 */
+const addTodo = () =>{
+if(todo){
+  setTodos([...todos,todo]);
+  setTodo('');//清空輸入框 為下一個待辦事項做準備
+}
+
+};
+
+  return (
+    <div className="App">
+
+      {/* 會在這裡添加待辦事項列表的UI界面代碼 */}
+    <input type="text" value={todo}  onChange={(e)=> setTodo(e.target.value)}/>
+<button onClick={addTodo}>添加</button>
+
 {/*添加代辦事項 */}
+<ul>
+{todos.map((item, index)=>(
+  <li key={index}>{item}</li> // 使用 map 方法來顯示每個待辦事項
+))}
+
+</ul>
     </div>
   );
 }
